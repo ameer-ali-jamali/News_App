ac346ee740938c587b08e8fbd4d68b6d38fdecdf
ameer-ali-jamali <ameer.ali.jamali886@gmail.com>
2023-05-31 23:31:11 +0500

    store function

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -7,15 +7,37 @@ use Illuminate\Http\Request;
 
 class ArticleController extends Controller
 {
+    //
     public function index()
     {
         $articles = Article::orderBy('id')->get();
         return response()->json($articles);
     }
-
+    //
     public function show($id)
     {
         $article = Article::findOrFail($id);
         return response()->json($article);
+    }
+    //
+    public function store(Request $request)
+    {
+
+        $validatedData = $request->validate([
+            'title' => 'required|max:30',
+            'content' => 'required',
+            'author' => 'required',
+            'category' => 'required',
+            'published_at' => 'required|date',
+
+        ]);
+        $article = new Article();
+        $article->title = $validatedData['title'];
+        $article->content = $validatedData['content'];
+        $article->author = $validatedData['author'];
+        $article->category = $validatedData['category'];
+        $article->published_at = $validatedData['published_at'];
+        $article->save();
+        return response()->json($article, 201);
     }
 }
